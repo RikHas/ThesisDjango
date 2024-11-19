9cb388da3aa180e51c462a9c186483fae8c93440
RikHas <skp47@mail.ru>
2024-11-19 21:55:03 +0300

    Update models.py

--- pythonDjango/mysite/records/models.py
+++ pythonDjango/mysite/records/models.py
@@ -3,9 +3,23 @@ from django.db import models
 
 # Create your models here.
 class Record(models.Model):
+    """
+    Класс для хранения записей.
+
+    С атрибутами:
+        title (str): Заголовок записи.
+        description (str): Описание записи.
+        created_at (datetime): Дата и время создания записи.
+    """
     title = models.CharField(max_length=100)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
+        """
+        Возвращает строковое представление записи.
+
+        Возврощает:
+            Заголовок записи.
+        """
         return self.title
